5972a12fea46a1c9fec0dd755c2a0922940f5f3c
alexanderspevak <alexander.spevak@gmail.com>
2018-10-24 12:33:14 +0200

    redo homework

--- index.js
+++ index.js
@@ -1,18 +1,25 @@
-const http=require('http');
+const http = require('http');
 const url = require('url')
-
-const server=http.createServer(function(req,res){
-    var parsedUrl=url.parse(req.url,true);
-    console.log('parsedUrl',parsedUrl)
-    if(parsedUrl.pathname==='/hello'){
-        res.setHeader('Content-Type','application/json')
+var StringDecoder = require('string_decoder').StringDecoder
+var decoder = new StringDecoder('utf-8')
+const server = http.createServer(function (req, res) {
+    var parsedUrl = url.parse(req.url, true);
+    res.setHeader('Content-Type', 'application/json')
+    if (parsedUrl.pathname === '/hello') {
         res.writeHead(400)
-        res.end(JSON.stringify({course:"excellent"}))
+        req.on('data', function (data) {
+            var yourMessage = '';
+            yourMessage = decoder.write(data);
+            res.end(JSON.stringify({ greeting: "Hello!", message: yourMessage }))
+        })
+        req.on('end', function () {
+            res.end(JSON.stringify({ greeting: "Hello!" }))
+        })
+    } else {
+        res.writeHead(200)
+        res.end(JSON.stringify({ youUsed: 'other route' }))
     }
-        res.end('other route')
 })
-
-server.listen(3000,function(){
+server.listen(3000, function () {
     console.log('server is listening on port 3000')
-})
-
+});
