b2e5f86868c7af5dd1ab6d4ad7fe31a0a134e850
alexanderspevak <alexander.spevak@gmail.com>
2018-12-16 16:04:04 +0100

    forking server

--- index.js
+++ index.js
@@ -1,25 +1,52 @@
 const http = require('http');
 const url = require('url')
+const os = require('os');
+const cluster = require('cluster')
 var StringDecoder = require('string_decoder').StringDecoder
 var decoder = new StringDecoder('utf-8')
-const server = http.createServer(function (req, res) {
-    var parsedUrl = url.parse(req.url, true);
-    res.setHeader('Content-Type', 'application/json')
-    if (parsedUrl.pathname === '/hello') {
-        res.writeHead(400)
-        req.on('data', function (data) {
-            var yourMessage = '';
-            yourMessage = decoder.write(data);
-            res.end(JSON.stringify({ greeting: "Hello!", message: yourMessage }))
-        })
-        req.on('end', function () {
-            res.end(JSON.stringify({ greeting: "Hello!" }))
-        })
-    } else {
-        res.writeHead(200)
-        res.end(JSON.stringify({ youUsed: 'other route' }))
+
+
+
+const startServer = function () {
+  
+    const server = http.createServer(function (req, res) {
+        var parsedUrl = url.parse(req.url, true);
+        res.setHeader('Content-Type', 'application/json')
+        if (parsedUrl.pathname === '/hello') {
+            res.writeHead(400)
+            req.on('data', function (data) {
+                var yourMessage = '';
+                yourMessage = decoder.write(data);
+                res.end(JSON.stringify({ greeting: "Hello!", message: yourMessage }))
+            })
+            req.on('end', function () {
+                res.end(JSON.stringify({ greeting: "Hello!" }))
+            })
+        } else {
+            res.writeHead(200)
+            res.end(JSON.stringify({ youUsed: 'other route' }))
+        }
+    })
+    server.listen(3000, function () {
+        console.log('server is listening on port 3000')
+    });
+}
+
+const init = function (callback) {
+    if (cluster.isMaster) {
+        for (var i = 0; i < os.cpus().length; i++) {
+            cluster.fork()
+        }
+      
+    }else{
+        startServer()
     }
-})
-server.listen(3000, function () {
-    console.log('server is listening on port 3000')
-});
+   
+    callback();
+}
+
+
+
+if(require.main===module){
+    init(function(){});
+}
